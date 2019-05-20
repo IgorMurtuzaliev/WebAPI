826eeb285104bd7c8a13a6b497dfda5c0efa7c0e
i.murtuzaliev <ingwarrior.99@yandex.ru>
2019-05-20 13:01:38 +0300

    Tokens generating

--- SCore.WebAPI/Controllers/ProductsController.cs
+++ SCore.WebAPI/Controllers/ProductsController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -25,7 +26,7 @@ namespace SCore.WebAPI.Controllers
             productService = _productService;
             _context = context;
         }
-
+        [Authorize]
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
         {
