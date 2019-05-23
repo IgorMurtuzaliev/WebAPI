59f91a673e6084c287f319cd06a4e2bdfbe61241
i.murtuzaliev <ingwarrior.99@yandex.ru>
2019-05-23 09:44:47 +0300

    Image permissions

--- SCore.WebAPI/Controllers/ProductsController.cs
+++ SCore.WebAPI/Controllers/ProductsController.cs
@@ -92,9 +92,28 @@ namespace SCore.WebAPI.Controllers
                 Price = model.Price,
                 Date = model.Date,
                 Description = model.Description,
-                ProductId = model.ProductId,
+                ProductId = model.ProductId,  
                 Images = model.Images
             };
+            const int lengthMax = 2097152;
+            const string correctType = "image/jpeg";
+            foreach (var image in model.Images)
+            {
+                var type = image.ContentType;
+                var length = image.Length;
+                if (type != correctType)
+                {
+                    ModelState.AddModelError("Uploads", "Error, allowed image resolution jpg / jpeg");
+                    return BadRequest(ModelState);
+                }
+
+                if (length < lengthMax) continue;
+                ModelState.AddModelError("Uploads", "Error, image size should not be more than 2 MB");
+                return BadRequest(ModelState);
+            }
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
             await productService.Create(product);
             return CreatedAtAction("GetProduct", new { id = product.ProductId }, product);
         }
